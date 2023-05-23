ed0f1d09352487c18fda7c88b0f5c07edb06f263
rudynakodach <whoppe7292@gmail.com>
2023-05-23 23:07:04 +0200

    code refactoring changes

---
+++ src/main/java/rudynakodach/github/io/webhookintegrations/Events/Actions/OpJoinEvent.java
@@ -0,0 +1,32 @@
+package rudynakodach.github.io.webhookintegrations.Events.Actions;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import rudynakodach.github.io.webhookintegrations.WebhookIntegrations;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class OpJoinEvent implements Listener {
+    Collection<String> opsJoined = new ArrayList<>();
+    private final JavaPlugin plugin;
+
+    public OpJoinEvent(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    private void onPlayerJoin(PlayerJoinEvent event) {
+        if (!opsJoined.contains(event.getPlayer().getName())) {
+            if (plugin.getServer().getOperators().contains(event.getPlayer())) {
+                if (!WebhookIntegrations.isLatest) {
+                    event.getPlayer().sendMessage(ChatColor.GRAY + "[" + ChatColor.BLUE + "W" + ChatColor.WHITE + "I" + ChatColor.GRAY + "]" + ChatColor.WHITE + " Update available. Please update from either GitHub, SpigotMC or Bukkit.");
+                    opsJoined.add(event.getPlayer().getName());
+                }
+            }
+        }
+    }
+}
